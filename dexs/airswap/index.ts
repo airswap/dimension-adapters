84a222172a115b11089df80a7cc9e5969d1bd72e
Perelyn <pisckid344@gmail.com>
2023-03-01 16:18:11 +0100

    update adapter

--- dexs/airswap/index.ts
+++ dexs/airswap/index.ts
@@ -1,8 +1,41 @@
+import axios from "axios";
+import { SimpleAdapter } from "../../adapters/types";
 import { CHAIN } from "../../helpers/chains";
-import { univ2Adapter } from "../../helpers/getUniSubgraphVolume";
+import { getUniqStartOfTodayTimestamp } from "../../helpers/getUniSubgraphVolume";
 
-const adapters = univ2Adapter({
-    [CHAIN.ETHEREUM]: "https://api.thegraph.com/subgraphs/name/airswap/airswap",
-}, {});
+const fetch = async (timestamp: number) => {
+    const dayTimestamp = getUniqStartOfTodayTimestamp(new Date(timestamp * 1000))
 
-export default adapters;
+    const url = 'https://gateway.thegraph.com/api/[api-key]/subgraphs/id/3xdURMor7NCcFs1g1Ff7JjnASQcgDyGsGY3Ba5n8VRDL';
+
+    const data = {
+      query: `
+      {
+        dailySwapVolumes(where: {date: ${dayTimestamp}}) {
+          amount
+        }
+      }
+      `
+    };
+
+    const dailyVolume = await (await axios.post(url, data)).data.data.dailySwapVolumes[0].amount.toString();
+
+    return {
+      totalVolume: "0" /**fix it */,
+      dailyVolume,
+      timestamp: dayTimestamp,
+    };
+    
+};
+
+
+const adapter: SimpleAdapter = {
+  adapter: {
+    [CHAIN.ETHEREUM]: {
+      fetch,
+      start: async () => 1649203200,
+    },
+  },
+};
+
+export default adapter;
